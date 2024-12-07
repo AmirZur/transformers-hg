e696196fca4f7f5efffebb4d43b530f1ca0e2a26
Amir Zur <amir.zur@hotmail.com>
2024-12-07 15:04:15 -0800

    release shared vocab run

--- scripts/train/tense_hier.sh
+++ scripts/train/tense_hier.sh
@@ -10,5 +10,6 @@ python train_transformers.py \
     --tied-embedding \
     --shared_vocab data_utils/shared_vocab/shared_vocab.json \
     --save_every 150000 \
-    --save_dir /nlp/scr/amirzur/checkpoint_tense_hier_s$seed \
-    --data_dir tense_inflection_hier_data
+    --save_dir /nlp/scr/amirzur/checkpoint_tense_hier_svocab_s$seed \
+    --data_dir tense_inflection_hier_data \
+    --wandb_dir /nlp/scr/amirzur
