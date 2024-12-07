c295e3b11459e850a0bdcb896014d51059c5d7f9
Amir Zur <amir.zur@hotmail.com>
2024-12-07 12:37:52 -0800

    re-run interrupted job (grr)

--- scripts/release/agreement_sm_hier_linear.sh
+++ scripts/release/agreement_sm_hier_linear.sh
@@ -1,6 +1,6 @@
 # agreement (small) hierarchical
-nlprun -a ellipse -g 1 -q jag -o logs/agreement_sm_hier_s0.log 'bash scripts/train/agreement_sm_hier.sh 0'
-nlprun -a ellipse -g 1 -q jag -o logs/agreement_sm_hier_s1.log 'bash scripts/train/agreement_sm_hier.sh 1'
+# nlprun -a ellipse -g 1 -q jag -o logs/agreement_sm_hier_s0.log 'bash scripts/train/agreement_sm_hier.sh 0'
+# nlprun -a ellipse -g 1 -q jag -o logs/agreement_sm_hier_s1.log 'bash scripts/train/agreement_sm_hier.sh 1'
 # agreement (large) linear
 nlprun -a ellipse -g 1 -q jag -o logs/agreement_sm_linear_s0.log 'bash scripts/train/agreement_sm_linear.sh 0'
-nlprun -a ellipse -g 1 -q jag -o logs/agreement_sm_linear_s1.log 'bash scripts/train/agreement_sm_linear.sh 1'
+# nlprun -a ellipse -g 1 -q jag -o logs/agreement_sm_linear_s1.log 'bash scripts/train/agreement_sm_linear.sh 1'
