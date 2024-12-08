cfd2d223148022bcfc3120d7fd06c26afa2c3c2f
Amir Zur <amir.zur@hotmail.com>
2024-12-07 21:35:35 -0800

    prepare to run transfer to tense experiments

--- scripts/release/transfer_agreement_lg_to_tense.sh
+++ scripts/release/transfer_agreement_lg_to_tense.sh
@@ -28,8 +28,8 @@ nlprun \
 # agree (large) linear to tense linear
 nlprun \
     -a ellipse -g 1 -q jag \
-    -o logs/agree_lg_linear_to_tense_linear_s0.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_tense_agreement_lg_s0 checkpoint_200000.pickle'
+    -o logs/agree_lg_linear_to_tense_linear_s0.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_agreement_lg_linear_s0 checkpoint_200000.pickle'
 
 nlprun \
     -a ellipse -g 1 -q jag \
-    -o logs/agree_lg_linear_to_tense_linear_s1.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_tense_agreement_lg_s1 checkpoint_200000.pickle'
+    -o logs/agree_lg_linear_to_tense_linear_s1.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_agreement_lg_linear_s1 checkpoint_200000.pickle'
--- scripts/release/transfer_agreement_sm_to_tense.sh
+++ scripts/release/transfer_agreement_sm_to_tense.sh
@@ -28,8 +28,8 @@ nlprun \
 # agree (small) linear to tense linear
 nlprun \
     -a ellipse -g 1 -q jag \
-    -o logs/agree_sm_linear_to_tense_linear_s0.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_tense_agreement_sm_s0 checkpoint_200000.pickle'
+    -o logs/agree_sm_linear_to_tense_linear_s0.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_agreement_sm_linear_s0 checkpoint_200000.pickle'
 
 nlprun \
     -a ellipse -g 1 -q jag \
-    -o logs/agree_sm_linear_to_tense_linear_s1.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_tense_agreement_sm_s1 checkpoint_200000.pickle'
+    -o logs/agree_sm_linear_to_tense_linear_s1.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_agreement_sm_linear_s1 checkpoint_200000.pickle'
