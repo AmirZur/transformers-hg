ec502949baf7442443d76036138b604b4f97a5a9
Amir Zur <amir.zur@hotmail.com>
2024-12-07 18:43:10 -0800

    this is why we debug people!

--- scripts/release/transfer_agreement_lg_to_tense.sh
+++ scripts/release/transfer_agreement_lg_to_tense.sh
@@ -1,35 +1,35 @@
 # agree (large) hierarchical to tense hierarchical
 nlprun \
     -a ellipse -g 1 -q jag \
-    -o logs/agree_lg_hier_to_tense_hier_s0.log 'bash scripts/transfer/tense_hier.sh /nlp/scr/amirzur/checkpoint_agreement_lg_hier_s0 checkpoint_200000.pt'
+    -o logs/agree_lg_hier_to_tense_hier_s0.log 'bash scripts/transfer/tense_hier.sh /nlp/scr/amirzur/checkpoint_agreement_lg_hier_s0 checkpoint_200000.pickle'
 
 nlprun \
     -a ellipse -g 1 -q jag \
-    -o logs/agree_lg_hier_to_tense_hier_s1.log 'bash scripts/transfer/tense_hier.sh /nlp/scr/amirzur/checkpoint_agreement_lg_hier_s1 checkpoint_200000.pt'
+    -o logs/agree_lg_hier_to_tense_hier_s1.log 'bash scripts/transfer/tense_hier.sh /nlp/scr/amirzur/checkpoint_agreement_lg_hier_s1 checkpoint_200000.pickle'
 
 # agree (large) linear to tense hierarchical
 nlprun \
     -a ellipse -g 1 -q jag \
-    -o logs/agree_lg_linear_to_tense_hier_s0.log 'bash scripts/transfer/tense_hier.sh /nlp/scr/amirzur/checkpoint_agreement_lg_linear_s0 checkpoint_200000.pt'
+    -o logs/agree_lg_linear_to_tense_hier_s0.log 'bash scripts/transfer/tense_hier.sh /nlp/scr/amirzur/checkpoint_agreement_lg_linear_s0 checkpoint_200000.pickle'
 
 nlprun \
     -a ellipse -g 1 -q jag \
-    -o logs/agree_lg_linear_to_tense_hier_s1.log 'bash scripts/transfer/tense_hier.sh /nlp/scr/amirzur/checkpoint_agreement_lg_linear_s1 checkpoint_200000.pt'
+    -o logs/agree_lg_linear_to_tense_hier_s1.log 'bash scripts/transfer/tense_hier.sh /nlp/scr/amirzur/checkpoint_agreement_lg_linear_s1 checkpoint_200000.pickle'
 
 # agree (large) hierarchical to tense linear
 nlprun \
     -a ellipse -g 1 -q jag \
-    -o logs/agree_lg_hier_to_tense_linear_s0.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_agreement_lg_hier_s0 checkpoint_200000.pt'
+    -o logs/agree_lg_hier_to_tense_linear_s0.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_agreement_lg_hier_s0 checkpoint_200000.pickle'
 
 nlprun \
     -a ellipse -g 1 -q jag \
-    -o logs/agree_lg_hier_to_tense_linear_s1.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_agreement_lg_hier_s1 checkpoint_200000.pt'
+    -o logs/agree_lg_hier_to_tense_linear_s1.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_agreement_lg_hier_s1 checkpoint_200000.pickle'
 
 # agree (large) linear to tense linear
 nlprun \
     -a ellipse -g 1 -q jag \
-    -o logs/agree_lg_linear_to_tense_linear_s0.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_tense_agreement_lg_s0 checkpoint_200000.pt'
+    -o logs/agree_lg_linear_to_tense_linear_s0.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_tense_agreement_lg_s0 checkpoint_200000.pickle'
 
 nlprun \
     -a ellipse -g 1 -q jag \
-    -o logs/agree_lg_linear_to_tense_linear_s1.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_tense_agreement_lg_s1 checkpoint_200000.pt'
+    -o logs/agree_lg_linear_to_tense_linear_s1.log 'bash scripts/transfer/tense_linear.sh /nlp/scr/amirzur/checkpoint_tense_agreement_lg_s1 checkpoint_200000.pickle'
