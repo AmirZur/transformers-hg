86802cf3a835d241ca4d051cc604d937fafa5571
Amir Zur <amir.zur@hotmail.com>
2024-12-09 08:57:46 -0800

    release agreement lg + tense experiments

--- scripts/release/multi_agreement_lg_and_tense.sh
+++ scripts/release/multi_agreement_lg_and_tense.sh
@@ -1,7 +1,7 @@
 # agreement (large) hierarchical
-nlprun -a ellipse -g 1 -q jag -o logs/agreement_lg_hier_to_tense_s0.log 'bash scripts/train/agreement_lg_hier_to_tense.sh 0'
-nlprun -a ellipse -g 1 -q jag -o logs/agreement_lg_hier_to_tense_s1.log 'bash scripts/train/agreement_lg_hier_to_tense.sh 1'
+nlprun -a ellipse -g 1 -q jag -o logs/multi_agreement_lg_hier_and_tense_s0.log 'bash scripts/multitask/agreement_lg_hier_and_tense.sh 0'
+nlprun -a ellipse -g 1 -q jag -o logs/multi_agreement_lg_hier_and_tense_s1.log 'bash scripts/multitask/agreement_lg_hier_and_tense.sh 1'
 # agreement (large) linear
-nlprun -a ellipse -g 1 -q jag -o logs/agreement_lg_linear_to_tense_s0.log 'bash scripts/train/agreement_lg_linear_to_tense.sh 0'
-nlprun -a ellipse -g 1 -q jag -o logs/agreement_lg_linear_to_tense_s1.log 'bash scripts/train/agreement_lg_linear_to_tense.sh 1'
+nlprun -a ellipse -g 1 -q jag -o logs/multi_agreement_lg_linear_and_tense_s0.log 'bash scripts/multitask/agreement_lg_linear_and_tense.sh 0'
+nlprun -a ellipse -g 1 -q jag -o logs/multi_agreement_lg_linear_and_tense_s1.log 'bash scripts/multitask/agreement_lg_linear_and_tense.sh 1'
 # TODO: agreement (large) ambiguous
