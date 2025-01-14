d57929491ee7bca68d24de5ede8f7fc12e1fa9e2
Amir Zur <amir.zur@hotmail.com>
2025-01-14 14:30:14 -0800

    continue informal experimentation by changing qf-tense ratio, hope for the best...

--- train_transformers.py
+++ train_transformers.py
@@ -448,7 +448,7 @@ def main_lm(args):
             splits=['train', 'val', 'test', 'linear']
         )
         # subsample from tense - the goal is to emphasize the qf task
-        tense_train = tense_data['train'].shuffle(seed=args.seed).select(range(1000))
+        tense_train = tense_data['train'].shuffle(seed=args.seed).select(range(10000))
 
         # combine the two datasets
         datasets = {
