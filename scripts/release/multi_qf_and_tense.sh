7c11261c8f04b5a7c01cf65cad0a95f0f482f971
Amir Zur <amir.zur@hotmail.com>
2025-01-13 17:37:22 -0800

    run all QF experiments

--- scripts/release/multi_qf_and_tense.sh
+++ scripts/release/multi_qf_and_tense.sh
@@ -1,9 +1,9 @@
 # # question formation hierarchical
-# nlprun -a ellipse -g 1 -q jag -o logs/multi_qf_hier_and_tense_s0.log 'bash scripts/multitask/qf_hier_and_tense.sh 0'
-# nlprun -a ellipse -g 1 -q jag -o logs/multi_qf_hier_and_tense_s1.log 'bash scripts/multitask/qf_hier_and_tense.sh 1'
+nlprun -a ellipse -g 1 -q jag -o logs/multi_qf_hier_and_tense_s0.log 'bash scripts/multitask/qf_hier_and_tense.sh 0'
+nlprun -a ellipse -g 1 -q jag -o logs/multi_qf_hier_and_tense_s1.log 'bash scripts/multitask/qf_hier_and_tense.sh 1'
 # # question formation linear
-# nlprun -a ellipse -g 1 -q jag -o logs/multi_qf_linear_and_tense_s0.log 'bash scripts/multitask/qf_linear_and_tense.sh 0'
-# nlprun -a ellipse -g 1 -q jag -o logs/multi_qf_linear_and_tense_s1.log 'bash scripts/multitask/qf_linear_and_tense.sh 1'
+nlprun -a ellipse -g 1 -q jag -o logs/multi_qf_linear_and_tense_s0.log 'bash scripts/multitask/qf_linear_and_tense.sh 0'
+nlprun -a ellipse -g 1 -q jag -o logs/multi_qf_linear_and_tense_s1.log 'bash scripts/multitask/qf_linear_and_tense.sh 1'
 # TODO: question formation ambiguous
 nlprun -a ellipse -g 1 -q jag -o logs/multi_qf_and_tense_s0.log 'bash scripts/multitask/qf_and_tense.sh 0'
 nlprun -a ellipse -g 1 -q jag -o logs/multi_qf_and_tense_s1.log 'bash scripts/multitask/qf_and_tense.sh 1'
