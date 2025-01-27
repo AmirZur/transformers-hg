a0d4834b3897df2f1d84ae660f8337ee533f4f81
Amir Zur <amir.zur@hotmail.com>
2025-01-27 15:59:57 -0800

    update launch script to focus on QF + tense aux

---
+++ scripts/release/multitask_ambig/qf_and_tense_aux.sh
@@ -0,0 +1,12 @@
+# all agreement
+nlprun -a ellipse -g 1 -q jag -o logs/qf_and_tense_aux_s0_r100.log 'bash scripts/multitask_ambig/qf_and_tense_aux.sh 0 100'
+nlprun -a ellipse -g 1 -q jag -o logs/qf_and_tense_aux_s1_r100.log 'bash scripts/multitask_ambig/qf_and_tense_aux.sh 1 100'
+nlprun -a ellipse -g 1 -q jag -o logs/qf_and_tense_aux_s2_r100.log 'bash scripts/multitask_ambig/qf_and_tense_aux.sh 2 100'
+# all tenseaux_
+nlprun -a ellipse -g 1 -q jag -o logs/qf_and_tense_aux_s0_r0.log 'bash scripts/multitask_ambig/qf_and_tense_aux.sh 0 0'
+nlprun -a ellipse -g 1 -q jag -o logs/qf_and_tense_aux_s1_r0.log 'bash scripts/multitask_ambig/qf_and_tense_aux.sh 1 0'
+nlprun -a ellipse -g 1 -q jag -o logs/qf_and_tense_aux_s2_r0.log 'bash scripts/multitask_ambig/qf_and_tense_aux.sh 2 0'
+# 50-50 mixaux_
+nlprun -a ellipse -g 1 -q jag -o logs/qf_and_tense_aux_s0_r50.log 'bash scripts/multitask_ambig/qf_and_tense_aux.sh 0 50'
+nlprun -a ellipse -g 1 -q jag -o logs/qf_and_tense_aux_s1_r50.log 'bash scripts/multitask_ambig/qf_and_tense_aux.sh 1 50'
+nlprun -a ellipse -g 1 -q jag -o logs/qf_and_tense_aux_s2_r50.log 'bash scripts/multitask_ambig/qf_and_tense_aux.sh 2 50'
