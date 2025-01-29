51f37bb14769f0b388147f5ac5fd79293de9d4e4
Amir Zur <amir.zur@hotmail.com>
2025-01-28 18:36:30 -0800

    set up tense -> agreement experiments

---
+++ scripts/transfer/agreement_lg_linear.sh
@@ -0,0 +1,19 @@
+from=$1
+cp=$2
+
+python train_transformers.py \
+    --encoder_n_layers 6 \
+    --callback \
+    --dataset simple_agreement \
+    --max_train_steps 100000 \
+    --eval_every 1000 \
+    --seed 42 \
+    --tied-embedding \
+    --save_every 100000 \
+    --save_dir ${from}_to_agree_lg_linear \
+    --grammar agreement_linear_v4 \
+    --data_dir grammar_gen_linear_data \
+    --model_load_path $from \
+    --model_load_checkpoint $cp \
+    --wandb_dir /nlp/scr/amirzur \
+    --shared_vocab tense
